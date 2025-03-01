6dc141823b717a22f784ffd82e0b7dd57efc26c0
nerlisse <allertalka@gmail.com>
2025-03-01 16:23:02 +0300

    initial commit

---
+++ src/main/java/ru/kaushina/dictionaryBot/DictionaryBotApplication.java
@@ -0,0 +1,13 @@
+package ru.kaushina.dictionaryBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kaushina/dictionaryBot/DictionaryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kaushina.dictionaryBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
