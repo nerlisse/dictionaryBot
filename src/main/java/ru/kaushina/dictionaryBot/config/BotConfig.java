c0c4e2f08567f687a8645088ab84dc2e85cee6ee
nerlisse <allertalka@gmail.com>
2025-03-26 00:20:24 +0300

    docs for config

--- src/main/java/ru/kaushina/dictionaryBot/config/BotConfig.java
+++ src/main/java/ru/kaushina/dictionaryBot/config/BotConfig.java
@@ -5,14 +5,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * Конфигурационный класс для настроек Telegram бота.
+ * <p>
+ * Загружает свойства бота (имя и токен) из файла {@code application.properties}
+ * и предоставляет к ним доступ через геттеры.
+ *
+ * @see Configuration
+ * @see PropertySource
+ */
 @Configuration
 @PropertySource("classpath:application.properties")
 @Getter
 public class BotConfig {
 
+    /**Имя бота, загружаемое из свойства {@code bot.name} в файле конфигурации.*/
     @Value("${bot.name}")
     private String botName;
 
+    /**Токен бота, загружаемый из свойства {@code bot.token} в файле конфигурации.
+     * * <b>Внимание:</b> Токен должен храниться в секрете и не попадать в публичные репозитории.*/
     @Value("${bot.token}")
     private String token;
 }
--- src/main/java/ru/kaushina/dictionaryBot/config/BotInitializer.java
+++ src/main/java/ru/kaushina/dictionaryBot/config/BotInitializer.java
@@ -10,14 +10,30 @@ import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.meta.generics.TelegramBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+/**
+ * Компонент для инициализации и регистрации Telegram бота при старте приложения.
+ * Регистрирует бота в Telegram API после полной инициализации контекста Spring.
+ * Обрабатывает ошибки регистрации бота.
+ */
 @Component
 public class BotInitializer {
 
     TelegramBot bot;
+
+    /**
+     * Конструктор для внедрения зависимости Telegram бота.
+     * @param bot экземпляр Telegram бота, который нужно зарегистрировать
+     */
     public BotInitializer(TelegramBot bot) {
         this.bot = bot;
     }
 
+    /**
+     * Регистрирует бота в Telegram API при событии обновления контекста Spring.
+     * Метод вызывается автоматически после полной инициализации контекста Spring.
+     * В случае ошибки регистрации выводит stacktrace в консоль.
+     * @throws TelegramApiException если произошла ошибка при регистрации бота
+     */
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
